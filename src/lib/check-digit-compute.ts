fc0d700d7e5ce83005de287b1fa1416cc6170df2
Emilio Platzer <emilioplatzer@gmail.com>
2023-08-05 19:37:10 -0300

    Agrego verificador de calidad.

--- src/lib/check-digit-compute.ts
+++ src/lib/check-digit-compute.ts
@@ -46,3 +46,70 @@ function checkdigitInternal<Num extends bigint|number>(number:any, cast:(numbreS
     return remainder;
 }
 
+export function checkQualityOfCodeList(list: string[], relative?:number){ 
+    var result = {
+        oneChanges: 0,
+        twoChanges: 0,
+        consecutiveSwap: {1:0,2:0,3:0},
+        anySwap: {1:0,2:0,3:0}
+    }
+    var denominator = {
+        oneChanges: 0,
+        twoChanges: 0,
+        consecutiveSwap: {1:0,2:0,3:0},
+        anySwap: {1:0,2:0,3:0}
+    }
+    var len = list[0].length
+    var vistos = {} as Record<string, boolean>
+    for (var codigo of list) {
+        vistos[codigo] = true;
+    }
+    for (var codigo of list) {
+        for (var i = 0; i < len; i++) {
+            for (var v = 0; v < 10; v++) {
+                var nuevo = codigo.substring(0, i) + v.toString() + codigo.substring(i+1);
+                if (nuevo != codigo) {
+                    if (v.toString() != codigo[i]){
+                        denominator.oneChanges ++;
+                        if (vistos[nuevo]) result.oneChanges ++;
+                        for (var j = 0; j < i; j++) {
+                            for (var w = 0; w < 10; w++) {
+                                var renuevo = nuevo.substring(0, j) + w.toString() + nuevo.substring(j+1);
+                                if (renuevo != codigo) {
+                                    if (w.toString() != codigo[j]){
+                                        denominator.twoChanges ++;
+                                        if (vistos[renuevo]) result.twoChanges ++;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            for (var l of [1,2,3] as (1|2|3)[]) {
+                if (i + l <= len) {
+                    for (var j = l as number; j <= i; j++) {
+                        var nuevo = codigo.substring(0, j-l) + codigo.substr(i,l) + codigo.substring(j,i) + codigo.substr(j-l,l) + codigo.substring(i+l);
+                        if (nuevo != codigo) {
+                            denominator.anySwap[l] ++;
+                            if (i == j) denominator.consecutiveSwap[l] ++;
+                            if (vistos[nuevo]) {
+                                result.anySwap[l] ++;
+                                if (i == j) result.consecutiveSwap[l] ++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    if (relative) {
+        result.oneChanges = result.oneChanges / denominator.oneChanges * relative;
+        result.twoChanges = result.twoChanges / denominator.twoChanges * relative;
+        for (var l of [1,2,3] as (1|2|3)[]) {
+            result.consecutiveSwap[l] = result.consecutiveSwap[l] / denominator.consecutiveSwap[l] * relative;
+            result.anySwap[l] = result.anySwap[l] / denominator.anySwap[l] * relative;
+        }
+    }
+    return result;
+}
--- src/test/test.ts
+++ src/test/test.ts
@@ -1,4 +1,4 @@
-import { checkdigit, checkdigitCompute, CheckDigitParameters } from "../lib/check-digit-compute";
+import { checkdigit, checkdigitCompute, CheckDigitParameters, checkQualityOfCodeList } from "../lib/check-digit-compute";
 
 import * as assert from "assert";
 import { IpcSocketConnectOpts } from "net";
@@ -63,6 +63,11 @@ describe("EAN13", function(){
         var result = checkdigitCompute(ean_wchd, EAN13);
         assert.equal(result, 8)
     });
+    it("checks EAN13", function(){
+        var ean = "4-003994-155486";
+        var result = checkdigit(ean, EAN13);
+        assert(result)
+    })
 })
 
 describe("bigint", function(){
@@ -112,5 +117,149 @@ describe("bigint", function(){
         var code = 111111111111111111n
         var result = checkdigitCompute(code, CONF)
         assert.equal(result, 1n);
+    })
+})
+
+describe("cuality measure", function(){
+    var shortList = [
+        "12340089",
+        "23450089",
+        "12350089", // simle error
+        "23540089", // consecutive swap
+        "32450089", // consecutive swap
+        "52430089", // any swap
+        "92340081", // any swap of extremmes
+        "50023489", // triple consecutive swap
+        "08950234", // triple swap
+        "12340067", // two simple errors
+    ];
+    it("check one change", function(){
+        var result = checkQualityOfCodeList(shortList);
+        assert.equal(result.oneChanges, 2)
+    })
+    it("check consecutive swap", function(){
+        var result = checkQualityOfCodeList(shortList);
+        assert.equal(result.consecutiveSwap[1], 4)
+    })
+    it("check any simple swap", function(){
+        var result = checkQualityOfCodeList(shortList);
+        assert.equal(result.anySwap[1], 8)
+    })
+    it("check shortList", function(){
+        var result = checkQualityOfCodeList(shortList);
+        assert.deepEqual(result, {
+            anySwap: {
+              "1": 8,
+              "2": 0,
+              "3": 4
+            },
+            consecutiveSwap: {
+              "1": 4,
+              "2": 0,
+              "3": 2
+            },
+            oneChanges: 2,
+            twoChanges: 12
+        })
+    })
+    it("check a long list with 1 check digit", function(){
+        const CONF:CheckDigitParameters<number> = {
+            cast: Number, 
+            multipliers: [2,3,5,7],
+            divider: 11,
+            turn: true
+        }
+        var cant = 0;
+        var i = 1000;
+        var list = [];
+        while(i<=9999){
+            var d1 = checkdigitCompute(i, CONF);
+            if(d1 != null){
+                var code = i + "" + d1;
+                list.push(code);
+                cant++;
+            }
+            i++
+        }
+        var result = checkQualityOfCodeList(list, 100);
+        assert.deepEqual(result,{
+            anySwap: {
+                "1": 0,
+                "2": 7.042253521126761,
+                "3": NaN
+              },
+              consecutiveSwap: {
+                "1": 0,
+                "2": 7.298168598669319,
+                "3": NaN
+              },
+              oneChanges: 0,
+              twoChanges: 9.121885704800277
+        })
+    })
+    it("check a long list with 2 check digit", function(){
+        const CONF1:CheckDigitParameters<number> = {
+            cast: Number, 
+            multipliers: [2,3,4,7],
+            divider: 11,
+            shift: 3
+        }
+        const CONF2:CheckDigitParameters<number> = {
+            cast: Number, 
+            multipliers: [3,4,5,9],
+            divider: 11
+        }
+        var cant = 0;
+        var i = 1000;
+        var list = [];
+        while(i<=9999){
+            var d1 = checkdigitCompute(i, CONF1);
+            var d2 = checkdigitCompute(i, CONF2);
+            if(d1 != null && d2 != null){
+                var code = i + "" + d1 + d2;
+                list.push(code);
+                cant++;
+            }
+            i++
+        }
+        var result = checkQualityOfCodeList(list, 100);
+        assert.deepEqual(result,{
+            anySwap: {
+                "1": 0,
+                "2": 1.3579883665663264,
+                "3": 0.5921938088829072
+              },
+              consecutiveSwap: {
+                "1": 0,
+                "2": 2.7161611588954275,
+                "3": 0.5921938088829072
+              },
+              oneChanges: 0,
+              twoChanges: 0
+        })
+    })
+    it("check a long list without check digit", function(){
+        var i = 1000;
+        var list = [];
+        while(i<=9999){
+            var code = i + "";
+            list.push(code);
+            i++
+        }
+        var result = checkQualityOfCodeList(list, 100);
+        assert.deepEqual(result,{
+            anySwap: {
+                "1": 94.44444444444444,
+                '2': 89.8989898989899,
+                "3": NaN
+              },
+              consecutiveSwap: {
+                "1": 96.29629629629629,
+                '2': 89.8989898989899,
+                "3": NaN
+              },
+              oneChanges: 97.22222222222221,
+              twoChanges: 94.44444444444444
+        })
     })
 })
